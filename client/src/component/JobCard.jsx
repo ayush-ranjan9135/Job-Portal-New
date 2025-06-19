faa0b1d857ec69b449c887026809f9f450b790ec
ayush-ranjan9135 <ayushherohai@gmail.com>
2025-06-19 14:29:27 +0530

    error-fix4

--- client/src/component/JobCard.jsx
+++ client/src/component/JobCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import JobListing from './JobListing'
 
 export const JobCard = ({job}) => {
 
