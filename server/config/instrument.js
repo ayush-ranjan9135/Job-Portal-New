d273c834a59499d81389809e6fc1e153709324d7
Ayush <ayushranjan9531@gmail.com>
2026-03-08 18:04:18 +0530

    debugged the database connectivity error

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -1,9 +1,9 @@
   // Import with if you are using ES
-  import * as Sentry from "@sentry/node"
-  import {nodeProfilingIntegration} from "@sentry/profiling-node";
+import * as Sentry from "@sentry/node"
+import {nodeProfilingIntegration} from "@sentry/profiling-node";
 
   Sentry.init({
-    dsn: "https://2bffd0a5da94536efdb8da3c4324ccb1@o4509462851944448.ingest.us.sentry.io/4509463024697344",
+    dsn: process.env.SENTRY_DSN,
 
     integrations:[
       nodeProfilingIntegration(),
